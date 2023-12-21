9fa643c2a825b4e01b864b8eb72d2a948c26ad66
Shiham123 <s36651786@gmail.com>
2023-12-21 21:47:51 +0600

    only from create to progree created

--- src/DashBroad/NewTask/TaskLists.jsx
+++ src/DashBroad/NewTask/TaskLists.jsx
@@ -1,15 +1,96 @@
 import { useQuery } from '@tanstack/react-query';
-import useAppContext from '../../Hooks/useAppContext';
 import usePublicApi from '../../Hooks/usePublicApi';
+import { useDrag, useDrop } from 'react-dnd';
+import PropTypes from 'prop-types';
+import { useEffect, useState } from 'react';
 
+const SectionProgress = () => {
+  const [progressData, setProgressData] = useState([]);
+  const publicApi = usePublicApi();
+
+  const [{ isOver }, drop] = useDrop(() => ({
+    accept: 'task',
+    drop: (item) => addItemToProgress(item.id),
+    collect: (monitor) => ({
+      isOver: !!monitor.isOver(),
+    }),
+  }));
+
+  const addItemToProgress = (id) => {
+    const requestBody = { status: 'ongoing' };
+    publicApi
+      .patch(`/tasks/${id}`, requestBody)
+      .then((response) => console.log(response))
+      .catch((error) => console.log(error));
+  };
+
+  useEffect(() => {
+    publicApi
+      .get('/ongoing')
+      .then((response) => {
+        console.log(response);
+        setProgressData(response.data);
+      })
+      .catch((error) => console.log(error));
+  }, [publicApi]);
+
+  return (
+    <div ref={drop} className={`min-h-screen ${isOver ? 'bg-green-200' : ''}`}>
+      {progressData.map((item, index) => {
+        const { title, description, priority } = item;
+        return (
+          <div
+            key={index}
+            className="group bg-lightTwo m-4 p-4 rounded-lg hover:bg-transparent border-2 border-lightTwo duration-300"
+          >
+            <h1 className="text-2xl capitalize font-lora">{title}</h1>
+            <p className="text-xl lowercase font-lora">{description}</p>
+            <p className="bg-darkThree inline-block px-4 py-2 border-2 border-darkThree text-lightOne text-xl uppercase font-bold font-lora group-hover:bg-transparent group-hover:duration-300 group-hover:text-darkOne">
+              {priority}
+            </p>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+const DraggableTask = ({ taskItem }) => {
+  const { _id, priority, description, title } = taskItem;
+
+  const [{ isDragging }, drag] = useDrag(() => ({
+    type: 'task',
+    item: { id: _id },
+    collect: (monitor) => ({
+      isDragging: !!monitor.isDragging(),
+    }),
+  }));
+
+  console.log(isDragging);
+
+  return (
+    <div
+      ref={drag}
+      key={_id}
+      className="group bg-lightTwo m-4 p-4 rounded-lg hover:bg-transparent border-2 border-lightTwo duration-300"
+    >
+      <h1 className="text-2xl capitalize font-lora">{title}</h1>
+      <p className="text-xl lowercase font-lora">{description}</p>
+      <p className="bg-darkThree inline-block px-4 py-2 border-2 border-darkThree text-lightOne text-xl uppercase font-bold font-lora group-hover:bg-transparent group-hover:duration-300 group-hover:text-darkOne">
+        {priority}
+      </p>
+    </div>
+  );
+};
+
+// ! main task
 const TaskLists = () => {
-  const { user } = useAppContext();
   const publicApi = usePublicApi();
 
   const { data: taskData = [] } = useQuery({
-    queryKey: [user?.email, 'task'],
+    queryKey: ['task'],
     queryFn: async () => {
-      const response = await publicApi.get(`/tasks/${user.email}`);
+      const response = await publicApi.get(`/created`);
       return response.data;
     },
   });
@@ -19,24 +100,12 @@ const TaskLists = () => {
       {/* div one */}
       <div className="col-span-3 border-2 border-lightOne min-h-screen">
         {taskData.map((item) => {
-          const { _id, priority, description, title } = item;
-          return (
-            <div
-              key={_id}
-              className="group bg-lightTwo m-4 p-4 rounded-lg hover:bg-transparent border-2 border-lightTwo duration-300"
-            >
-              <h1 className="text-2xl capitalize font-lora">{title}</h1>
-              <p className="text-xl lowercase font-lora">{description}</p>
-              <p className="bg-darkThree inline-block px-4 py-2 border-2 border-darkThree text-lightOne text-xl uppercase font-bold font-lora group-hover:bg-transparent group-hover:duration-300 group-hover:text-darkOne">
-                {priority}
-              </p>
-            </div>
-          );
+          return <DraggableTask key={item._id} taskItem={item} />;
         })}
       </div>
       {/* div two */}
       <div className="col-span-3 border-2 border-lightTwo min-h-screen">
-        <h2>Div two</h2>
+        <SectionProgress />
       </div>
       {/* div three */}
       <div className="col-span-3 border-2 border-lightThree min-h-screen">
@@ -44,6 +113,10 @@ const TaskLists = () => {
       </div>
     </div>
   );
+};
+
+DraggableTask.propTypes = {
+  taskItem: PropTypes.object.isRequired,
 };
 
 export default TaskLists;
