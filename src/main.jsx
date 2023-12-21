4ce9b8c24937fab60574a79868c1edafc44f60ef
Shiham123 <s36651786@gmail.com>
2023-12-21 17:21:56 +0600

    queryClient wrap main jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,13 @@ import './main.css';
 import { RouterProvider } from 'react-router-dom';
 import router from './Routes/Router.jsx';
 import { AppProvider } from './AppContext/Context.jsx';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <AppProvider>
-    <RouterProvider router={router} />
-  </AppProvider>
+  <QueryClientProvider client={queryClient}>
+    <AppProvider>
+      <RouterProvider router={router} />
+    </AppProvider>
+  </QueryClientProvider>
 );
