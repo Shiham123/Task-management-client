938cbad7c58953ac7c1c2e5a6f8f15894701b8df
Shiham123 <s36651786@gmail.com>
2023-12-22 00:29:55 +0600

    drag and drop complete only one way

--- src/DashBroad/NewTask/TaskLists.jsx
+++ src/DashBroad/NewTask/TaskLists.jsx
@@ -3,6 +3,62 @@ import usePublicApi from '../../Hooks/usePublicApi';
 import { useDrag, useDrop } from 'react-dnd';
 import PropTypes from 'prop-types';
 
+const CompletedProgress = () => {
+  const publicApi = usePublicApi();
+  const [{ isOver }, drop] = useDrop(() => ({
+    accept: 'task',
+    drop: (item) => addToCompleted(item.id),
+    collect: (monitor) => ({
+      isOver: !!monitor.isOver(),
+    }),
+  }));
+
+  const addToCompleted = (id) => {
+    const requestBody = { status: 'completed' };
+    publicApi
+      .patch(`/tasks/${id}`, requestBody)
+      .then((response) => console.log(response))
+      .catch((error) => console.log(error));
+  };
+
+  const { data: completedData = [] } = useQuery({
+    queryKey: ['complete'],
+    queryFn: async () => {
+      const response = await publicApi.get('/completed');
+      console.log(response);
+      return response.data;
+    },
+  });
+
+  // TODO: I have to add here useEffect and retch when i am going to deploy
+  // useEffect(() => {
+  //   const intervalId = setInterval(() => {
+  //     refetch();
+  //   }, 500);
+  //   return () => clearInterval(intervalId);
+  // }, [refetch]);
+
+  return (
+    <div ref={drop} className={`min-h-screen ${isOver ? 'bg-green-200' : ''}`}>
+      {completedData.map((item, index) => {
+        const { title, description, priority } = item;
+        return (
+          <div
+            key={index}
+            className="group bg-lightTwo m-4 p-4 rounded-lg hover:bg-transparent border-2 border-lightTwo duration-300"
+          >
+            <h1 className="text-2xl capitalize font-lora">{title}</h1>
+            <p className="text-xl lowercase font-lora">{description}</p>
+            <p className="bg-darkThree inline-block px-4 py-2 border-2 border-darkThree text-lightOne text-xl uppercase font-bold font-lora group-hover:bg-transparent group-hover:duration-300 group-hover:text-darkOne">
+              {priority}
+            </p>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
 const SectionProgress = () => {
   const publicApi = usePublicApi();
 
@@ -63,15 +119,13 @@ const SectionProgress = () => {
 const DraggableTask = ({ taskItem }) => {
   const { _id, priority, description, title } = taskItem;
 
-  const [{ isDragging }, drag] = useDrag(() => ({
+  const [, drag] = useDrag(() => ({
     type: 'task',
     item: { id: _id },
     collect: (monitor) => ({
       isDragging: !!monitor.isDragging(),
     }),
   }));
-
-  console.log(isDragging);
 
   return (
     <div
@@ -122,7 +176,7 @@ const TaskLists = () => {
       </div>
       {/* div three */}
       <div className="col-span-3 border-2 border-lightThree min-h-screen">
-        <h1>Div three</h1>
+        <CompletedProgress />
       </div>
     </div>
   );
