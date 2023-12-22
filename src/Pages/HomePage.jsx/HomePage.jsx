47a740942f74446bbf3a4be4236e972757a300ce
Shiham123 <s36651786@gmail.com>
2023-12-22 18:22:29 +0600

    some section added

--- src/Pages/HomePage.jsx/HomePage.jsx
+++ src/Pages/HomePage.jsx/HomePage.jsx
@@ -1,12 +1,14 @@
 import ClientSection from '../../Component/Client/ClientSection';
 import FaqSection from '../../Component/Faq/FaqSection';
 import HeroSection from '../../Component/Hero/HeroSection';
+import ServicesSection from '../../Component/Services/ServicesSection';
 
 const HomePage = () => {
   return (
     <div>
       <HeroSection />
       <ClientSection />
+      <ServicesSection />
       <FaqSection />
     </div>
   );
